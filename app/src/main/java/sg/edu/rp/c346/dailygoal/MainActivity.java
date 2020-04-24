d68dd00e86ddda78c4fcec690e8454887358f7d5
18016080 <18016080@myrp.edu.sg>
2020-04-24 18:00:04 +0800

    Completed all except advanced enhancement

--- app/src/main/java/sg/edu/rp/c346/dailygoal/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/dailygoal/MainActivity.java
@@ -2,7 +2,13 @@ package sg.edu.rp.c346.dailygoal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RadioGroup rg1 = (RadioGroup) findViewById(R.id.radioGroup1);
+                RadioGroup rg2 = (RadioGroup) findViewById(R.id.radioGroup2);
+                RadioGroup rg3 = (RadioGroup) findViewById(R.id.radioGroup3);
+                int selectedButtonId1 = rg1.getCheckedRadioButtonId();
+                int selectedButtonId2 = rg3.getCheckedRadioButtonId();
+                int selectedButtonId3 = rg2.getCheckedRadioButtonId();
+                RadioButton rb1 = (RadioButton) findViewById(selectedButtonId1);
+                RadioButton rb2 = (RadioButton) findViewById(selectedButtonId2);
+                RadioButton rb3 = (RadioButton) findViewById(selectedButtonId3);
+                EditText ed = (EditText) findViewById(R.id.editText);
+
+                String[] info = {rb1.getText().toString(),rb2.getText().toString(), rb3.getText().toString(), ed.getText().toString()};
+                Intent i = new Intent(MainActivity.this, Summary.class);
+                i.putExtra("info",info);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
+
+                // Get the Id of the selected radio button in the RadioGroup
+
+                // Get the radio button object from the Id we had gotten above
--- app/src/main/java/sg/edu/rp/c346/dailygoal/Summary.java
+++ app/src/main/java/sg/edu/rp/c346/dailygoal/Summary.java
@@ -2,7 +2,11 @@ package sg.edu.rp.c346.dailygoal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Summary extends AppCompatActivity {
 
@@ -10,5 +14,35 @@ public class Summary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        // Get the intent so as to get the "things" inside the intent
+        Intent i = getIntent();
+        // Get the String array named "info" we passed in
+        String[] info = i.getStringArrayExtra("info");
+        // Get the TextView object
+        TextView tv1 = findViewById(R.id.textView8);
+        TextView tv2 = findViewById(R.id.textView9);
+        TextView tv3 = findViewById(R.id.textView10);
+        TextView tv4 = findViewById(R.id.textView11);
+        Button close = findViewById(R.id.button2);
+        // Display the name and age on the TextView
+        tv1.setText("Read up on materials before class : " + info[0]);
+        tv2.setText("Arrive on time so as not to miss important part of the lesson : " + info[1]);
+        tv3.setText("Attempt the problem myself :" + info[2]);
+        tv4.setText("Refection : " + info[3]);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent s = new Intent(Summary.this, MainActivity.class);
+
+                startActivity(s);
+
+            }
+        });
+
+
+
     }
 }
